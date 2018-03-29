0fcf0fca6caa189d123c993d3593251d3cf07fa9
Soumye <soumye@iitk.ac.in>
2018-03-29 16:26:26 +0530

    added assignment 4

--- Assignment2/3b/queryGoogleNumPages.py
+++ Assignment2/3b/queryGoogleNumPages.py
@@ -3,21 +3,21 @@ import time
 import math
 
 def queryGoogleNumPages(query):
-	offset = 23;
+	offset = 23
 	qstring = 'https://www.google.co.in/search?source=hp&ei=-ZWFWrPvGY20vwTv9ZGgAQ&q=' + query
 
 	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
 	req = urllib.request.Request(qstring, headers = headers)
 	page = urllib.request.urlopen(req)
 	s = page.read().decode('utf-8')
-	sIdx = s.find('id="resultStats"');
+	sIdx = s.find('id="resultStats"')
 	# print(s[sIdx:])
-	eIdx = s[sIdx:].find(' results');
+	eIdx = s[sIdx:].find(' results')
 	# eIdx = eIdx[1];
 
-	rstring = s[sIdx+offset:sIdx+eIdx-1];
+	rstring = s[sIdx+offset:sIdx+eIdx-1]
 	rstring = rstring.replace(',','')
-	out = math.log(float(rstring));
+	out = math.log(float(rstring))
 
 	time.sleep(2)           # very important if you don't want to seem like a bot and have your IP blocked by Google
 	return float(rstring)
